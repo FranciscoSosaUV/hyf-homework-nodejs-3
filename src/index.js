d4a9d3eb3e64dfdaceedcde23f976e788c85fbe2
Francisco Sosa <fsosa@unitedvirtualities.com>
2019-12-02 11:56:57 -0700

    server express is running

---
+++ src/index.js
@@ -0,0 +1,17 @@
+const express = require('express'); //da acceso a la biblioteca express buscando en sus node_modules "express"
+const app = express(); // crea una instancia del constructor express, al que llamaremos "aplicación".
+
+const bodyParser = require('body-parser');
+app.use(bodyParser.json());
+
+
+app.get('/', function(req,res){ //GET 
+    res.json({
+         success: true,
+         message: 'Hello world!'
+    })
+})
+
+app.listen(3000,function(){
+    console.log("server is running")  // iniciará el servidor localmente en el puerto que proporcione como primer argumento 
+    })
